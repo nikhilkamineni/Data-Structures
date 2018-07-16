c323758e3c026fcba8a91d694d6bd9e75057770d
Nikhil Kamineni <nikhil.kamineni@gmail.com>
2018-07-16 18:06:20 -0400

    Completed add_to_tail and remove_head in Linked List

--- linked_list/linked_list.py
+++ linked_list/linked_list.py
@@ -3,6 +3,7 @@ Class that represents a single linked
 list node that holds a single value
 and a reference to the next node in the list
 """
+# Node class
 class Node:
   def __init__(self, value=None, next_node=None):
     self.value = value
@@ -17,19 +18,37 @@ class Node:
   def set_next(self, new_next):
     self.next_node = new_next
 
+# Linked List class
 class LinkedList:
   def __init__(self):
     self.head = None
     self.tail = None
 
   def add_to_tail(self, value):
-    pass
+    new_tail = Node(value)
+
+    if self.head is None:
+      self.head = new_tail
+
+    if self.tail is not None:
+      self.tail.next_node = new_tail
+
+    self.tail = new_tail
+
 
   def remove_head(self):
+    if self.head is not None:
+      old_head = self.head
+      if old_head.next_node is not None:
+        self.head = old_head.next_node
+      else:
+        self.head = None
+      return old_head.value
+
+
+  def contains(self, value):
     pass
 
-  def contains(self):
-    pass
 
-  def get_max(self):
+  def get_max(self, value):
     pass
